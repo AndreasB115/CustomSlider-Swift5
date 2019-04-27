c290fb2ee81309d9b8ab8ad079082efca5f39969
Fanni Boros <topdev999@hotmail.com>
2019-04-27 16:19:35 +0200

    first commit

---
+++ SwiftProject/CustomBtn.swift
@@ -0,0 +1,21 @@
+//
+//  CustomBtn.swift
+//  SwiftProject
+//
+//  Created by Piglet on 09.04.19.
+//  Copyright © 2019 Dev. All rights reserved.
+//
+
+import UIKit
+
+class CustomBtn: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
